2676f6e80b7e176612416a175f941b808696dd1f
atsuya23 <90808047+atsuya23@users.noreply.github.com>
2021-09-23 12:24:05 +0900

    ナベアツ(*_*; state変更のたびに再読み込みが発生するので無限ループに注意！

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,12 @@ const App = () => {
     setFaceShowFlag(!faceShowFlag);
   };
 
+  if (num % 3 === 0) {
+    faceShowFlag || setFaceShowFlag(true);
+  } else {
+    faceShowFlag && setFaceShowFlag(false);
+  }
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
